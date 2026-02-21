10165aa52cf09fee36588e1dd9e430e09073b201
chrry03 <ocr925@dgu.ac.kr>
2026-02-22 08:31:52 +0900

    로컬주소 취소

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -7,7 +7,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://d2hixyxntmembw.cloudfront.net/api', 
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
 })
